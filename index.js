117a36f1292a438658b1a478219673e54c455574
123falksdfj <changnan666@gmail.com>
2022-10-08 18:45:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+const { existsSync } = require("node:fs");
+const path = require("node:path");
+const { exec } = require("child_process");
+const notifier = require("node-notifier");
+
+const cwd = process.cwd();
+const gitDir = path.resolve(cwd, ".git");
+
+if (existsSync(gitDir)) {
+  const execCommand = (command) => {
+    return new Promise((resolve, rejected) => {
+      exec(command, { cwd }, (error, stdout) => {
+        if (error) {
+          rejected(`error: ${error.message}`);
+          return;
+        }
+
+        resolve(stdout);
+      });
+    });
+  };
+
+  let needPush = false;
+  let loading = false;
+
+  const commit = () =>
+    execCommand(
+      `git add . && git commit -m "[${new Date().toLocaleString()}] sync"`
+    );
+
+  const action = async () => {
+    if (loading) return;
+    loading = true;
+
+    await execCommand("git fetch");
+
+    const result = await execCommand("git status");
+
+    if (result.indexOf("modified:") !== -1) {
+      needPush = true;
+      await commit();
+    }
+
+    try {
+      const pullRecord = await execCommand("git pull --rebase");
+
+      if (pullRecord.indexOf("Updating") !== -1) {
+        const [, ...msg] = (
+          await execCommand("git log -1 --pretty=oneline")
+        ).split(" ");
+
+        notifier.notify({
+          title: "已同步至本地",
+          message: msg.join(" "),
+        });
+      }
+    } catch (error) {
+      console.error("pull 失败" + error);
+      await commit();
+    }
+
+    if (needPush) {
+      try {
+        await execCommand("git push");
+        const [, ...msg] = (
+          await execCommand("git log -1 --pretty=oneline")
+        ).split(" ");
+
+        notifier.notify({
+          title: "已同步至仓库",
+          message: msg.join(" "),
+        });
+      } catch (error) {
+        console.error("push 失败" + error);
+      }
+
+      needPush = false;
+    }
+
+    loading = false;
+  };
+
+  setInterval(action, 1000);
+} else {
+  console.warn("同步失败，缺少 .git 目录");
+}
