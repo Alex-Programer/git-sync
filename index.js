dc87a47fb800edc4280de6c6584df93135c8a4c3
coder-lcn <changnan666@gmail.com>
2022-11-01 05:16:46 +0800

    [2022/11/1 05:16:45] sync

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const { fork } = require("node:child_process");
 const path = require("path");
 
-const subProcess = path.resolve(__dirname, "./sync.js");
+const subProcess = path.resolve(__dirname, "./auto-sync.js");
 
 const subprocess = fork(subProcess, {
   cwd: process.cwd(),
