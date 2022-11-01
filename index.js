6350488ff5993973597308933d5b6efc7361ac0f
coder-lcn <changnan666@gmail.com>
2022-11-01 09:07:04 +0800

    [2022/11/1 09:07:04] sync

--- index.js
+++ index.js
@@ -2,17 +2,12 @@
 const { program } = require("commander");
 
 program
-  .option("-d, --debug", "output extra debugging")
-  .option("-s, --small", "small pizza size")
-  .option("-p, --pizza-type <type>", "flavour of pizza");
-
+  .option("-l, --list", "show sync list")
+  .option("-d, --delete", "delete a sync task");
 program.parse(process.argv);
 
 const options = program.opts();
-if (options.debug) console.log(options);
-console.log("pizza details:");
-if (options.small) console.log("- small pizza size");
-if (options.pizzaType) console.log(`- ${options.pizzaType}`);
+if (options.list) console.log("123");
 
 // const { fork } = require("node:child_process");
 // const path = require("path");
